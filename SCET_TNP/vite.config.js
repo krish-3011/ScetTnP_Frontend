6605f71f9eee40f39afacd0c0bfbebf0406ab5e2
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-07-26 19:48:59 +0530

    new commit

--- SCET_TNP/vite.config.js
+++ SCET_TNP/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.', // Add this line to set the root directory
   plugins: [react()],
 })
