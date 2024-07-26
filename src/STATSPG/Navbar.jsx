7983cba8b97d269916698368d257a9d2636cee1c
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-07-26 22:24:16 +0530

    new commit

--- src/STATSPG/Navbar.jsx
+++ src/STATSPG/Navbar.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './style.css';
 import './header.css';
 import './button.css';
 import './graph.css';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
 
+const Navbar = () => {
   return (
     <div className="nav">
       <div className="logotext">
-        <img className="logo" src="./source1/sesuni.png" alt="SU LOGO" />
+        <img className="logo" src="source1/sesuni.png" alt="SU " />
+
         <div className="text1">
           <p className="scettext inter-font">SCET</p>
           <div className="line-1"></div>
@@ -25,7 +26,7 @@ const Navbar = () => {
       <div className="side-bar">
         {["STATISTICS", "VISION_AND_MISSION", "TnP_POLICY", "ABOUT", "OFFERS", "TEAM", "NOTIFICATION", "PROFILE","ADD_OFFER"].map((item, index) => (
           <div key={index} className={`frame-${index + 3}`}>
-            <div className={`${item.toLowerCase().replace(/ & /g, '-').replace(/ /g, '-')}-inter-font`} id={item.toLowerCase().replace(/ /g, '-')} onClick={() => window.location.href = `/${item.toLowerCase().replace(/ & /g, '-').replace(/ /g, '-')}`}><Link to={`/${item.toLowerCase()}`}className="no-underline">{item}</Link></div>
+            <div className={`${item.toLowerCase().replace(/ & /g, '-').replace(/ /g, '-')}-inter-font`} id={item.toLowerCase().replace(/ /g, '-')} onClick={() => window.location.href = `/${item.toLowerCase().replace(/ & /g, '-').replace(/ /g, '-')}`}><Link to={`/${item.toLowerCase()}`} className="no-underline">{item}</Link></div>
           </div>
         ))}
       </div>
