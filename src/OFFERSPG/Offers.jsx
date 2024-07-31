16fd9a5448f33a263528a8e165f626eb127b3ea6
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-07-31 18:47:42 +0530

    profile page

--- src/OFFERSPG/Offers.jsx
+++ src/OFFERSPG/Offers.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Navbar from "../STATSPG/Navbar";
 import Header from "../STATSPG/Header";
 import Offerscontainer from "./Offercontainer";
 import "./cards.css";
 import LoginPage from "./Login";
+import Cookies from 'js-cookie'; // Import js-cookie
 
 const Offers = () => {
   const [showLoginPage, setShowLoginPage] = useState(true);
+  const [profileData, setProfileData] = useState(null);
 
-  const handleLoginSubmit = (success) => {
+  const handleLoginSubmit = async (success) => {
     if (success) {
-      setShowLoginPage(false); // Hide the LoginPage if login is successful
+      const userCookie = Cookies.get('user'); // Read session cookie
+      if (userCookie) {
+        const data = JSON.parse(userCookie);
+        setProfileData(data);
+        setShowLoginPage(false);
+      }
     } else {
-      // Keep LoginPage visible if login fails
       setShowLoginPage(true);
     }
   };
+
+  useEffect(() => {
+    const userCookie = Cookies.get('user'); // Read session cookie
+    if (userCookie) {
+      const data = JSON.parse(userCookie);
+      setProfileData(data);
+    } else {
+      setShowLoginPage(true);
+    }
+  }, []);
 
   return (
     <>
